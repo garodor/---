a5d7ec4cf54cded3334734b85344379a006bddd7
garodor <90151484+garodor@users.noreply.github.com>
2023-12-20 01:04:39 +0300

    Add files via upload

---
+++ Zadacha 10/Cart.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+class Cart {
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
